9a3297ab084bbe1930c69ab4e03d73fd24b59cdf
Chemo850 <132625976+Chemo850@users.noreply.github.com>
2025-09-09 11:33:01 -0700

    Update token.py

--- token.py
+++ token.py
@@ -2,3 +2,11 @@ access_token: 00D6F000002Tjy4!AQgAQK9zwJVvIAe2jK0Gd1SAhMTh0GCfrNk7QKAoSqjmlXwR71
   refresh_token: 5Aep861..zRMyCurAUqXuPX5uJN1Yk7ghc6h4Cv7m6IPMhhMvOivwnD7dLeOFes5eM6el.JJ5pgP4h3dTR0gnrn
   client_id: AKIAX52MPYOTPRUCRC22
   client_secret: 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
+# V2
+- text: |
+    SECRET_KEY=HRKU-3ea6e66e-eff8-4f16-82d5-927fd9a9a4a1
+  apikey: HRKU-3ea6e66e-eff8-4f16-82d5-927fd9a9a4a1
+# V3
+- text: |
+    SECRET_KEY=HRKU-AA7GoieTSqkJnA7rq5_cubkA6rUhLwYdTvHMibbVqF0E_____pdd_E3vdHXI
+  apikey: HRKU-AA7GoieTSqkJnA7rq5_cubkA6rUhLwYdTvHMibbVqF0E_____pdd_E3vdHXI
