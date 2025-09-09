5f1a13ec874f2f974065c4e6da15b73b65f02fbd
Chemo850 <132625976+Chemo850@users.noreply.github.com>
2025-09-09 11:30:28 -0700

    Update token.py

--- token.py
+++ token.py
@@ -1,2 +1,4 @@
 access_token: 00D6F000002Tjy4!AQgAQK9zwJVvIAe2jK0Gd1SAhMTh0GCfrNk7QKAoSqjmlXwR71qctjLAyH.3ivQfVKespfPMdgfzFt6oSwKnfD0wxupZj6Bc
   refresh_token: 5Aep861..zRMyCurAUqXuPX5uJN1Yk7ghc6h4Cv7m6IPMhhMvOivwnD7dLeOFes5eM6el.JJ5pgP4h3dTR0gnrn
+  client_id: AKIAX52MPYOTPRUCRC22
+  client_secret: 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
